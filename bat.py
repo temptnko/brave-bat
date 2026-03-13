f73dfc0dfc15a9143d4a593ddffb9972733455a0
temptnko <frun.tmain@tutamail.com>
2026-03-13 23:26:04 +0100

    updating bat.py setiing up prompt for a user to set custom monitor resolution

--- bat.py
+++ bat.py
@@ -2,9 +2,10 @@ import pyautogui
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from time import sleep
-from random import randint
+from random import randint, choice
 import datetime
 
+random_char = random.choice('abcdefghijklmnopqrstuvwxyz')
 
 def get_first_word(filename):
     with open(filename, 'r') as file: 
@@ -23,7 +24,7 @@ def FakeBrowse():
     pyautogui.click()
     pyautogui.move(randint(x, y), randint(x, y))
     sleep(2)
-    pyautogui.type(randint([a-z])) # simulate accidental click
+    pyautogui.typewrite(random_char) # simulate accidental click
     sleep(3)
     pyautogui.move(43)
     sleep(2)
@@ -33,38 +34,39 @@ def FakeBrowse():
 
 
 def timer():
-    date = datetime.now()
-    if int(date) % 2 == 0:
-        return True
-    else:
-       return False
+    current_time = datetime.datetime.now()
+    return current_time.hour % 2 == 0 
 
+def OpenBrowser():
+    options = webdriver.ChromeOptions() #this shi allows me to config this mf
+    options.binary_location = "/usr/bin/brave-browser"
+    driver = webdriver.Chrome(service=ChromeDriverManager().install(), options=options) #lwk dont understand but launches brave
+    is_browser_open = True
+    return driver
 
-options = webdriver.ChromeOptions() #this shi allows me to config this mf
-options.binary_location = "/usr/bin/brave-browser"
-driver = webdriver.Chrome(service=ChromeDriverManager().install(), options=options) #lwk dont understand but launches brave
+is_browser_open = False
 
-is_browser_open = True
 
 while True:
     if timer():
         if not is_browser_open:
-            options = webdriver.ChromeOptions()
-            options.binary_location = "/usr/bin/brave-browser"
-            driver = webdriver.Chrome(service=ChromeDriverManager().install(), options=options)
-            is_browser_open = True
+            try:
+                driver = OpenBrowser()  
+                is_browser_open = True
+            except Exception as e:
+                print("Error opening the browser:", e)
+    else:
+        if is_browser_open:
+            driver.quit() 
+            is_browser_open = False
 
+    if is_browser_open: 
         for word in words:
-            pyautogui.moveTo(x, y)
+            pyautogui.moveTo(randint(0, x), randint(0, y))  
             pyautogui.click()
             pyautogui.typewrite(word)
             pyautogui.press('enter')
             sleep(randint(6, 10))
             FakeBrowse()
 
-    else:
-        if is_browser_open:  
-            driver.quit()
-            is_browser_open = False
-           
-        sleep(60) 
+    sleep(60)
